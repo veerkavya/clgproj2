300edacb5e15fd8b81e12dedd00ce2db46bb27e4
kavya <106902650+veerkavya@users.noreply.github.com>
2022-08-20 02:16:29 +0530

    Update e.js

--- e.js
+++ e.js
@@ -1,4 +1,38 @@
 const h1=document.getElementById("hh");
+const scrolbtn=document.getElementsByClassName("ab")[1];
+const bt=document.getElementsByClassName("ab")[0];
+const pc2=document.getElementsByClassName("pc2")[0];
+let j=0;
+scrolbtn.addEventListener("click",function(){
+    pc2.scrollBy(100,0);
+    console.log(0)
+    j+=100;
+   
+    if(j>0){
+        
+        bt.classList.add("o1")
+        bt.classList.remove("o0");
+      
+       
+    }
+     console.log(0)
+
+})
+bt.addEventListener("click",function(){
+    pc2.scrollBy(-100,0);
+    console.log(0)
+    j-=100;
+   
+    if(j===100){
+        
+        bt.classList.add("o0")
+        bt.classList.remove("o1");
+      
+       
+    }
+     console.log(0)
+
+})
 const scroll=["skills","passion","knowledge"];
 var i=0;
 setInterval(()=>{
@@ -11,4 +45,3 @@ setInterval(()=>{
         
     },1500)
     
-
